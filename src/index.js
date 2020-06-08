31685a1d20d95b8fb539b37f98b27612794d14af
Unknown <jillzyt25@gmail.com>
2020-06-08 20:34:25 +0800

    Removed Redux tools
    Removed Redux Tools

--- src/index.js
+++ src/index.js
@@ -20,8 +20,7 @@ const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
-    reduxFirestore(firebase),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    reduxFirestore(firebase)
   )
 );
 
