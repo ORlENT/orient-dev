3e337694d844c6fc0e9aca9e621045074b698283
Unknown <jillzyt25@gmail.com>
2020-06-09 12:59:22 +0800

    Added Session Variable for Announcements
    Added Session variable for announcements

--- src/Pages/Camp/Announcements.js
+++ src/Pages/Camp/Announcements.js
@@ -3,37 +3,70 @@ import { Header, CenterBox, SummaryCard } from "../../UI";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import { firestoreConnect, isLoaded, isEmpty } from "react-redux-firebase";
+import { Button } from "@material-ui/core";
 
 class Announcements extends Component {
   render() {
-    const { announcements } = this.props;
+    let { announcements } = this.props;
+    let announcementsList = JSON.parse(sessionStorage.getItem("announcements"));
 
+    // Announcements loading
     if (!isLoaded(announcements)) {
       return <div>Loading</div>;
     }
 
+    // Set session variables for annoucements
+    if (!announcementsList) {
+      sessionStorage.setItem("announcements", JSON.stringify(announcements));
+      announcementsList = JSON.parse(sessionStorage.getItem("announcements"));
+    }
+
+    // Render the announcements
     return (
       <CenterBox>
         <Header>Announcements</Header>
-        {isEmpty(announcements) && (
-          <Header>No announcement was not found</Header>
+        {isEmpty(announcementsList) && (
+          <Header>No announcement was not found.</Header>
         )}
-        {announcements &&
-          Object.keys(announcements).map((key) => (
-            <SummaryCard
-              key={key}
-              title={announcements[key].title}
-              content={announcements[key].content}
-              timestamp={announcements[key].timestamp}
-            />
+        {announcementsList &&
+          Object.keys(announcementsList).map((key) => (
+            <React.Fragment>
+              <SummaryCard
+                key={key}
+                title={announcementsList[key].title}
+                content={announcementsList[key].content}
+                timestamp={announcements[key].timestamp}
+                read={announcementsList[key].readStatus}
+              />
+              <Button
+                onClick={() => {
+                  announcementsList[key].readStatus = true;
+                  sessionStorage.setItem(
+                    "announcements",
+                    JSON.stringify(announcementsList)
+                  );
+                  this.forceUpdate();
+                }}
+              >
+                Button to read the above announcement.
+              </Button>
+            </React.Fragment>
           ))}
       </CenterBox>
     );
   }
 }
 
+const setReadStatusFalse = (data) => {
+  return Object.keys(data).map((item) => ({
+    ...data[item],
+    readStatus: false,
+  }));
+};
+
 const mapStateToProps = (state) => {
-  const announcements = state.firestore.data.announcements;
+  let announcements = state.firestore.data.announcements;
+  if (announcements) announcements = setReadStatusFalse(announcements);
   return { announcements };
 };
 
@@ -44,11 +77,6 @@ const firestoreQuery = () =>
       doc: props.match.params.id,
       subcollections: [{ collection: "announcements" }],
       storeAs: "announcements",
-    },
-    {
-      collection: "camps",
-      doc: props.match.params.id,
-      storeAs: "camp",
     },
   ]);
 
